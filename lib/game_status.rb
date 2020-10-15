9bb05c72c1805ffcf9e827706fd0ca89eed8af5d
Delia Constantino <dconstan92@gmail.com>
2020-10-15 15:45:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,12 +28,11 @@ def won?(board)
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combination # return the win_combination indexes that won.
     end
-
   end
   false
 end
 
-def full?(board)
-  board.all? do |index|
-if position_taken?(board, index)
-end
+# def full?(board)
+#   board.all? do |index|
+# if position_taken?(board, index)
+# end
