42411e20bc940d9b84343ac96994eff856a4a649
Delia Constantino <dconstan92@gmail.com>
2020-10-15 17:23:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -59,7 +59,10 @@ def winner(board)
   if board[positions_array[0]].to_s == "X"|| board[positions_array[0]].to_s == "O"
     return board[positions_array[0]].to_s
   end
-  nil
+  outcome = draw?(board)
+  if outcome == true || positions_array == false
+    return nil
+  end
 end
 
 
